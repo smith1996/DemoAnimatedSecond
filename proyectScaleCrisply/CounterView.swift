b76215967d0e9c6376b4f5b27ce1d531bf364fc3
Smith Huamani <smith.huamani@tismart.com>
2017-03-30 23:18:49 -0500

    Se avanzo el demo

---
+++ proyectScaleCrisply/CounterView.swift
@@ -0,0 +1,21 @@
+//
+//  CounterView.swift
+//  proyectScaleCrisply
+//
+//  Created by Raul Huamani on 30/03/17.
+//  Copyright © 2017 Smith Huamani. All rights reserved.
+//
+
+import UIKit
+
+class CounterView: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
